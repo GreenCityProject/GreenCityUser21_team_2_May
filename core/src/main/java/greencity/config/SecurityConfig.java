3004f4dc59a5032877759639832339035565d214
K4sik <kasiks597@gmail.com>
2024-05-28 23:26:04 +0300

    316 Fixed bug [PUT /user/updateUserLastActivityTime/{date}] response 401 Unauthorized - need 403 Forbidden and added test case

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -171,6 +171,7 @@ public class SecurityConfig {
                                 "/user/employee-email")
                         .hasAnyRole(USER, ADMIN, UBS_EMPLOYEE, MODERATOR, EMPLOYEE)
                         .requestMatchers(HttpMethod.PUT,
+                                "/user/updateUserLastActivityTime/{date}",
                                 "/user/edit-authorities",
                                 "/user/authorities",
                                 "/user/deactivate-employee",
