aa315f08f746b2e424e95e1f6c5c20ba9378b56c
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2024-05-29 00:53:56 +0300

    279 add test case for UserController /user/findUserForManagement status 403, bug was fixed earlier

--- core/src/test/java/greencity/controller/UserControllerWithSecurityConfigTest.java
+++ core/src/test/java/greencity/controller/UserControllerWithSecurityConfigTest.java
@@ -175,4 +175,12 @@ public class UserControllerWithSecurityConfigTest {
                 .andExpect(status().isUnauthorized());
         verifyNoInteractions(userService);
     }
+
+    @Test
+    @WithMockUser(username = "testmail@mail.com", roles = "USER")
+    void findUserForManagementByPage_isForbidden() throws Exception {
+        mockMvc.perform(get(userLink + "/findUserForManagement"))
+                .andExpect(status().isForbidden());
+        verifyNoInteractions(userService);
+    }
 }
